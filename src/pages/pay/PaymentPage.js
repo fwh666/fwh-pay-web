5ab334c9fd5e78da8c7fb3156335c5931b915be0
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-03-30 17:18:36 +0800

    fix:详情直接支付

--- src/pages/pay/PaymentPage.js
+++ src/pages/pay/PaymentPage.js
@@ -1,41 +1,36 @@
 import React, {useState} from 'react';
 import './PaymentPage.css';
-import {Button} from 'antd'
+import {Button, Form, Input, Space} from 'antd'
 import Header from "../home/header";
 import Footer from "../home/footer";
-import instance from '../../utils/network';
-import Alipay from "./Alipay";
-import {useNavigate} from 'react-router-dom';
 
 
 function PaymentPage() {
-    /*重定向变量*/
-    // const [redirect, setRedirect] = useState(false);
-    // const history = useHistory();
-
-    //定义跳转函数
-    const navigate = useNavigate();
-    function handleBuyClick() {
-        navigate("/alipay");
+    //定义变量
+    const [formData, setFormData] = useState({name: ''});
+    //支付页面调用
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const url = `/api/pay/getWebPay?email=${formData.name}`;
+        fetch(url)
+            .then(response => response.text())
+            .then((data) => {
+                console.log('data:', data);
+                const formElement = document.createElement("div");
+                formElement.style.display = "display:none;";
+                formElement.innerHTML = data;//去到from
+                if (formElement.firstChild) {
+                    formElement.firstChild.target = "_self";
+                    document.body.appendChild(formElement);
+                    formElement.firstChild.submit();//进行跳转
+                    document.body.removeChild(formElement);
+                }
+            })
+            .catch(error => console.log(error));
     }
 
 
     const [amount, setAmount] = useState(19.9); // 订单金额
-    let qr_code = ''
-    /*二维码链接*/
-    const pay = () => {
-        // 调用支付接口
-        instance.get('/api/pay/getPayQrCode').then((response) => {
-            console.log('响应：', response);
-            qr_code = response
-        })
-    }
-
-    /*支付页面*/
-    const payment = () => {
-        instance.get('/api/pay/getWebPay')
-
-    }
 
     return (
         <main>
@@ -43,31 +38,43 @@ function PaymentPage() {
                 <Header/>
             </header>
             <div className="Payment">
-                {/*<div className="QR_code">*/}
-                {/*    <QRCode value={qr_code}/>*/}
-                {/*</div>*/}
                 <div className="PayTip">
                     <p>支付宝扫码支付：¥{amount}</p>
                 </div>
                 <div className="PayTip">
                     <p>支付方式：支付宝扫码</p>
                 </div>
-                <div className="PayTip">
-                    <p>订单编号： Z23032815809JKLA</p>
-                    <p>商品名称： VPN特惠账号</p>
-                    <p>购买数量： 1</p>
-                </div>
-                {/*如何调用接口回显重定向页面？TODO-fwh-填写邮箱入口发送账号信息*/}
-                <Button onClick={handleBuyClick} type="primary" size="large">
-                    确认付款
-                </Button>
+
+                <Form onFinish={handleSubmit}>
+                    <Form.Item>
+                        <div className="PayTip">
+                            <p>订单编号： Z23032815809JKLA</p>
+                            <p>商品名称： VPN特惠账号</p>
+                            <p>购买数量： 1</p>
+                            <p><span className="font_red">*</span>接收账号邮箱：
+                                <Space direction={"vertical"} size={"middle"}>
+                                    <Input placeholder="example@gmail.com"
+                                           defaultValue={"zhangsan@163.com"}
+                                           value={formData.name}
+                                           onChange={(e) => setFormData({...formData, name: e.target.value})}
+                                           type={"email"}
+                                           required
+                                           size={"large"}/>
+                                </Space>
+                            </p>
+                        </div>
+                        {/*<Button onClick={handleBuyClick} type="submit" size="large">*/}
+                        <Button onClick={handleSubmit} type="submit" size="large">
+                            确认付款
+                        </Button>
+                    </Form.Item>
+                </Form>
             </div>
             <footer>
                 <Footer/>
             </footer>
         </main>
-    )
-        ;
+    );
 }
 
 export default PaymentPage;
